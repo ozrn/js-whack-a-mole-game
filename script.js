afadcb5555246d5a5039ee000e00e610bbad5909
ozrn <ozerenkevser92@gmail.com>
2022-12-10 12:37:06 +0300

    Get random hole from nodelist of holes

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ const moles = document.querySelectorAll(".mole");
 function getRandomTime(min, max){
     return Math.round(Math.random() * (max - min) + min);
 }
+
+function getRandomHole(holes){
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+    console.log(hole);
+}
   
