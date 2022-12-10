f8b54f4cb0fd95717b0337b8074dda4c4d6b53cf
ozrn <ozerenkevser92@gmail.com>
2022-12-10 12:56:45 +0300

    Update fn not to get same hole twice in a row

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+let lastHole;
 
 function getRandomTime(min, max){
     return Math.round(Math.random() * (max - min) + min);
@@ -9,6 +10,11 @@ function getRandomTime(min, max){
 function getRandomHole(holes){
     const index = Math.floor(Math.random() * holes.length);
     const hole = holes[index];
-    console.log(hole);
+    if(hole === lastHole){
+        return getRandomHole(holes);
+    }
+
+    lastHole = hole;
+    return hole;
 }
   
