04c5b0e85d82ee4dbb6fa7c305dc5fe582002f93
ozrn <ozerenkevser92@gmail.com>
2022-12-11 12:10:38 +0300

    Increment score as moles get clicked

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
 let lastHole;
 let timeUp = false;
+let score = 0;
 
 function getRandomTime(min, max){
     return Math.round(Math.random() * (max - min) + min);
@@ -33,7 +34,17 @@ function popUpMoles(){
 function startGame(){
     scoreBoard.textContent = 0;
     timeUp = false;
+    score = 0;
     popUpMoles();
     setTimeout(() => timeUp = true, 10000);
 }
+
+function bonk(e){
+    if(!e.isTrusted) return; // fake clicking!
+    score++;
+    this.classList.remove("up");
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener("click", bonk));
   
